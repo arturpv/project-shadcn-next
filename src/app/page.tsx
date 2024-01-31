4930266c6de7860e9542d650f92a53bff27a0100
Artur <desenvolvimento@1niq.com>
2024-01-31 17:03:31 -0300

    V 0.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,17 @@ import {
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 interface Instruments {
   title: string,
@@ -52,8 +63,23 @@ export default async function Home() {
               <p>{instrument.description}</p>
             </CardContent>
             <CardFooter className="flex justify-between">
-              <Button>Link to Buy</Button>
-              {instrument.acoustic && <Badge variant="secondary">ACOUSTIC!</Badge>}
+             <AlertDialog>
+                <AlertDialogTrigger><Button>Link to Buy</Button></AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                      You will send to another site!
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction>Continue</AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+              
+                {instrument.acoustic && <Badge variant="secondary">ACOUSTIC!</Badge>}
             </CardFooter>
           </Card>
         ))}
